71ecaa8832f977698c4337c4da6146e7619e595e
hitesh <hitesh.yerekar@gmail.com>
2025-06-24 16:39:24 +0530

    added updated code.

--- loaders/image_ocr_loader.py
+++ loaders/image_ocr_loader.py
@@ -4,4 +4,5 @@ import pytesseract
 
 def extract_text_from_image(image_path):
     image = Image.open(image_path)
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
     return pytesseract.image_to_string(image)
